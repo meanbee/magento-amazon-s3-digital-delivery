bb6aa295788ad97efd53cbea2a7fb19172a56f19
Nick Jones <nick@nicksays.co.uk>
2011-06-17 18:02:08 +0100

    Add configuration, js injection on the product edit page

--- code/Helper/Config.php
+++ code/Helper/Config.php
@@ -1,4 +1,70 @@
 <?php
 class Meanbee_DigitalDelivery_Helper_Config extends Mage_Core_Helper_Abstract {
-    
+    const XML_GENERAL_ENABLED   = 'mbdd/general/enabled';
+    const XML_GENERAL_PROVIDER  = 'mbdd/general/provider';
+
+    const XML_AMAZON_ACCESS     = 'mbdd/aws/access_key';
+    const XML_AMAZON_SECRET     = 'mbdd/aws/secret_key';
+    const XML_AMAZON_URLS       = 'mbdd/aws/urls';
+
+    /**
+     * Is the complete module enabled
+     *
+     * @return bool
+     */
+    public function isEnabled() {
+        return $this->_getStoreConfigFlag(self::XML_GENERAL_ENABLED);
+    }
+
+    /**
+     * The currently selected provider from the configuration dropdown
+     *
+     * @return string
+     */
+    public function getProvider() {
+        return $this->_getStoreConfig(self::XML_GENERAL_PROVIDER);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAmazonAccessKey() {
+        return $this->_getStoreConfig(self::XML_AMAZON_ACCESS);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAmazonSecretKey() {
+        return $this->_getStoreConfig(self::XML_AMAZON_SECRET);
+    }
+
+    /**
+     * @return array
+     */
+    public function getAmazonUrls() {
+        $urls = $this->_getStoreConfig(self::XML_AMAZON_URLS);
+
+        if (!empty($urls)) {
+            return explode(",", $urls);
+        }
+
+        return array();
+    }
+
+    /**
+     * @param  $xml_path
+     * @return string
+     */
+    protected function _getStoreConfig($xml_path) {
+        return Mage::getStoreConfig($xml_path, Mage::app()->getStore()->getCode());
+    }
+
+    /**
+     * @param  $xml_path
+     * @return bool
+     */
+    protected function _getStoreConfigFlag($xml_path) {
+        return Mage::getStoreConfigFlag($xml_path, Mage::app()->getStore()->getCode());
+    }
 }
---
+++ code/Model/Provider/Aws.php
@@ -0,0 +1,11 @@
+<?php
+class Meanbee_DigitalDelivery_Model_Provider_Aws extends Meanbee_DigitalDelivery_Model_Provider_Abstract {
+    protected $_service = null;
+
+    public function isConfigured() {
+        $key_access = $this->_getConfig()->getAmazonAccessKey();
+        $key_secret = $this->_getConfig()->getAmazonSecretKey();
+
+        return !empty($key_access) && !empty($key_secret);
+    }
+}
