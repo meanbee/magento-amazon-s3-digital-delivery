28041491ba3ba4f8db23e81b821e1097fa639fa9
Nick Jones <nick@nicksays.co.uk>
2011-06-20 18:12:52 +0100

    Remove the need for URLs, and add a message to the product edit/new page

--- code/Block/Adminhtml/Product/Edit/Js.php
+++ code/Block/Adminhtml/Product/Edit/Js.php
@@ -1,9 +1,5 @@
 <?php
 class Meanbee_DigitalDelivery_Block_Adminhtml_Product_Edit_Js extends Mage_Core_Block_Template {
-    public function getAmazonUrls() {
-        return json_encode($this->_getConfig()->getAmazonUrls());
-    }
-
     /**
      * @return Meanbee_DigitalDelivery_Helper_Config
      */
